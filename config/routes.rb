55409f7ccea897f5791384ca7ff798ee13d2cdf6
evillarta <edvil0816@gmail.com>
2016-10-30 06:11:13 +0800

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ 
   root 'page#home'
+
   get 'about', to: 'page#about'
+
+  resources :articles
+
+
 end
